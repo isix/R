38d46fc4278cc2b658cfe25b019c56c144466439
Isaias Prestes <isaias.prestes@gmail.com>
2018-04-19 18:06:29 +0800

    Adjusted parameter in code.

--- Data/data_generator_002.r
+++ Data/data_generator_002.r
@@ -1,5 +1,22 @@
-dataset <- data.frame(category = rep(LETTERS[1:5], 100),
-    x = rnorm(500, mean = rep(1:5, 100)),
-    y = rnorm(500, mean = rep(1:5, 100)))
+n <- 2000
+dataset <- data.frame(category = rep(LETTERS[1:5], n),
+    x = rnorm(500, mean = rep(1:5, n)),
+    y = rnorm(500, mean = rep(1:5, n)))
 dataset$fCategory <- factor(dataset$category)
 subdata <- subset(dataset, category %in% c("A", "D", "E"))
+setwd('C:/Users/Isaias Prestes/Documents/Pesquisas/GitHub/R/Data')
+write.table(subdata, file = "Data_IRF_20100312.dat", sep = "\t", row.names = 0)
+
+
+n <- 2000
+dataset <- data.frame(z = rnorm(500, mean = rep(1:5, n)),
+    x = rnorm(500, mean = rep(1:5, n)),
+    y = rnorm(500, mean = rep(1:5, n)))
+subdata <- dataset
+setwd('C:/Users/Isaias Prestes/Documents/Pesquisas/GitHub/R/Data')
+write.table(subdata, file = "Data_IRF_20100312.dat", sep = "\t", row.names = FALSE)
+
+
+## License
+
+This project is licensed under the terms of the MIT license, see LICENSE.
